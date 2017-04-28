76ef896a6377a47bcae9d5fd8a8461fb118a3ad1
josiahhopkins <hopkinsjosiah@gmail.com>
2017-04-28 14:30:00 -0700

    Fixed some syntax errors in Conference Tester

--- 360GroupProject/src/tests/ConferenceTest.java
+++ 360GroupProject/src/tests/ConferenceTest.java
@@ -14,17 +14,16 @@ public class ConferenceTest {
 
     @Test
     void testGetSubmissionDeadline() {
-        Date submissionDeadline = new Date();
+        Date submissionDeadline = new Date().;
         Date reviewDeadline = new Date();
-        Conference tester = new Conference(submissionDeadline, reviewDeadline,
-                );
+        Conference tester = new Conference(submissionDeadline, reviewDeadline, new List<Reviewer>());
         asserEquals(submissionDeadline, tester.getSubmissionDeadline);
 
 
         Date submissionDeadline2 = new Date();
         Date reviewDeadline2 = new Date();
-        tester = new Conference(submissionDeadline2, reviewDeadline2,
-                );
+        tester = new Conference(submissionDeadline2, reviewDeadline2, new List<Reviewer>()
+        );
         assertEquals(submissionDeadline2, tester.getSubmissionDeadline);
     }
 
@@ -33,8 +32,8 @@ public class ConferenceTest {
 
         Date submissionDeadline = new Date();
         Date reviewDeadline = new Date();
-        Conference tester = new Conference(submissionDeadline, reviewDeadline,
-                );
+        Conference tester = new Conference(submissionDeadline, reviewDeadline, new List<Reviewer>()
+        );
         assertEquals(reviewDeadline, tester.getReviewDeadline);
 
         Date submissionDeadline = new Date();
@@ -48,16 +47,16 @@ public class ConferenceTest {
     @Test
     void getPastReviewers() {
 
-        Date submissionDeadline = new Date();
-        Date reviewDeadline = new Date();
-        Conference tester = new Conference(submissionDeadline, reviewDeadline, new ArrayList<Reviewer>()
-                );
-        assertEquals(tester.getPastReviewers(), new ArrayList<Reviewer()>)
+        Date submissionDeadline = Calendar.getTime();
+        Date reviewDeadline = Calendar.getTime();
+        Conference tester = new Conference(submissionDeadline, reviewDeadline, new ArrayList<Reviewer>());
+        assertEquals(tester.getPastReviewers(), new ArrayList<Reviewer() >)
     }
 
     @Test
     void testForSubmitManuscriptAndGetManuscripts() {
-        Conference tester = new Conference(Calendar.getTime(), )
+        Conference tester = new Conference(Calendar.getInstance.addDays(CalendarDate, 1).getTime(), Calendar.getTime(),
+                new List<Reviewer>());
         Manuscript testManuscript = new Manuscript();
         List<Manuscript> theManuscripts = new ArrayList<Manuscript>();
         theManuscripts.add(testManuscript);
@@ -71,18 +70,16 @@ public class ConferenceTest {
 
     @Test
     void testForSubmitManuscriptDeadlineVerification() {
+        Conference tester = new Conference(Calendar.getInstance.addDays(CalendarDate, -1).getTime(), Calendar.getTime(),
+                new List<Reviewer>());
         Manuscript testManuscript = new Manuscript();
         List<Manuscript> theManuscripts = new ArrayList<Manuscript>();
-        theManuscripts.add(testManuscript);
         tester.submitManuscript(testManuscript);
         assertEquals(tester.getManuscripts, theManuscripts);
 
         Manuscript secondManuscript = new Manuscript();
-        theManuscripts.add(secondManuscript);
         assertEquals(tester.getManuscripts, theManuscripts);
     }
-
-
 
 
 }
