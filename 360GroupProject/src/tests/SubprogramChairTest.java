892676c5c716c5c9af82a08f34243a110f7a74ed
BlueAccords <myoldemailname@gmail.com>
2017-04-29 01:47:31 -0700

    added some logic for subprogram chair tests

--- 360GroupProject/src/tests/SubprogramChairTest.java
+++ 360GroupProject/src/tests/SubprogramChairTest.java
@@ -3,6 +3,9 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 
  * @author Ryan Tran.
@@ -12,8 +15,8 @@ import org.junit.Test;
 public class SubprogramChairTest {
     
 	// fixtures
-    // private ArrayStack<Integer> myStack;
 	private List<Reviewer> myReviewerList;
+	private SubprogramChair mySubprogramChair;
 
     /**
      * Initialize the test fixture before each test.
@@ -21,13 +24,45 @@ public class SubprogramChairTest {
     @Before
     public void setUp() {
     	myReviewerList = new List<Reviewer>();
+        mySubprogramChair = new SubprogramChair("Marauder");
     }
 
+
+    /**
+     * Test method for SubprogramChair constructor
+     */
+    @Test
+    public void testConstructor() {
+        String testUsername = "Scion";
+        SubprogramChair simpleChairObj = new SubprogramChair(testUsername);
+
+        AssertEquals(simpleChairObj.getName(), testUsername);
+    }
+
+    /**
+     * Test method for SubprogramChair#getConferenceList
+     */
+    @Test
+    public void testGetConferenceList() {
+        List<Conference> conferenceListWithMoreThanOne = new ArrayList<Conference>();
+        Conference tempConfA = new Conference(new Date(), new Date(), new List<Reviewer>());
+        Conference tempConfB = new Conference(new Date(), new Date(), new List<Reviewer>());
+        Conference tempConfC = new Conference(new Date(), new Date(), new List<Reviewer>());
+
+        // initialize conference list to have multiple items
+        conferenceListWithMoreThanOne.add(tempConfA);
+        conferenceListWithMoreThanOne.add(tempConfB);
+        conferenceListWithMoreThanOne.add(tempConfC);
+
+        mySubprogramChair.setConferences(conferenceListWithMoreThanOne);
+
+        AssertEquals(mySubprogramChair.getConfernceList(), conferenceListWithMoreThanOne);
+    }
     /**
      * Test method for SubprogramChair#getReviewers()
      */
     @Test
-    public void testGetReviewers() {
-    	
-    }
+    public void testGetReviewers() {}
+
+
 }
