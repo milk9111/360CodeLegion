ff321cd313a6a5cdd230a4ff75a6968dfb183235
cga3798 <fightnyank@live.com>
2017-05-05 18:43:51 -0700

    Started UI class

--- 360GroupProject/src/UI.java
+++ 360GroupProject/src/UI.java
@@ -1,4 +1,28 @@
 
 public class UI {
+	private int myState;
+	private String userType;
+	public void changeState() {
+			
+		if (Controller.theState < 10) {
+			setUserType("Author");
+			displayHeader();
+		} else {
+			setUserType("SubProgramChair");
+			displayHeader();
+		}
+	}
+	
+	private void setUserType(String theUserType) {
+		userType = theUserType;
+	}
+	private void displayHeader() {
+		System.out.println();
+		
+	}
+	
+	private void AuthorView() {
+		
+	}
 
 }
