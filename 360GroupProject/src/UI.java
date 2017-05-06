41e242b130951cad8c470712d05bc9fd24f2c4a3
MDanger <clearwaters@gmail.com>
2017-05-06 14:30:57 -0700

    strings added to notify observers

--- 360GroupProject/src/UI.java
+++ 360GroupProject/src/UI.java
@@ -18,26 +18,26 @@ import model.User;
  */
 
 public class UI extends Observable implements Observer{
-	
-	
+
+
 	//View States
 	public final int AUTHOR = 0;
 	public final int REVIEWER = 10;
 	public final int SUBPROGRAM_CHAIR = 20;
-	
+
 	//Action States
 	public final int SUBMIT_MANUSCRIPT = 1;
 	public final int LIST_MANUSCRIPT_VIEW = 2;
 	public final int LIST_CONFERENCE_VIEW = 3;
 	public final int ASSIGN_REVIEWER = 4;
-	
+
 	private int myState;
 	private String userType;
 	private String myUserName;
 	private User myUser;
 	private Scanner myScanner;
 	private int myUserChoice;
-	
+
 	public UI() {
 		myState = 0;
 		userType = "";
@@ -45,73 +45,73 @@ public class UI extends Observable implements Observer{
 		myUserChoice = 0;
 		myScanner = new Scanner(System.in);
 	}
-	
+
 	/**
-     * The start point for the program.
-     * 
-     * @param theArgs command line arguments - ignored
-     */
-    public static void main(final String[] theArgs) {
-        new UI().start();
-    }
-    /**
-     * Login prompt.  Displays prompt and scans input, compares to list of existing users.
-     * If valid, updates this UI user fields and proceeds to top menuL chooseUserTypeMenuView 
-     * 
-     * @author Casey Anderson, Morgan Blackmore
-     * 
-     */
-	
-    private void start() {
-    	boolean validUserName = false;
-    	while (!validUserName) {
-	    	System.out.println("Please enter user name to log in: ");
-	    	myUserName = myScanner.next();
-	    	if (myUser.isUserRegistered(myUserName)) {
-	    		validUserName = true;
-	    		
-	    		chooseUserTypeMenuView();
-	    	} else {
-	    		System.out.println("Invalid User Name!");
-	    	}
-    	}
-    	
-    }
-    
-    /**
-     * Top menu from login.  
-     * User chooses how they want to proceed which notifies Observer with string
-     * 
-     * @author Morgan Blackmore
-     * 
-     */
-    private void chooseUserTypeMenuView() {
-    	
-    	System.out.println("\nChoose what type of user you are");
-    	System.out.println("1 - Author");
-    	System.out.println("2 - SubProgram Chair");
-<<<<<<< HEAD
-    	if (myScanner.next()== "1") {
-    		notifyObservers("AUTHOR"); 
-    	} else if(myScanner.next()=="2"){
-    		notifyObservers("SUBPROGRAM_CHAIR"); 
-=======
-    	if (myScanner.next() == "1") {
-    		notifyObservers("Author"); 
-    	} else if(myScanner.next() =="2"){
-    		notifyObservers("SubProgram Chair"); 
->>>>>>> branch 'master' of https://github.com/milk9111/360CodeLegion/
-      	} else {
-      		System.out.println("Invalid choice, please select from the options displayed");
-      		chooseUserTypeMenuView();
-      	}
-    	
-    	
-    	
-    }
-    
+	 * The start point for the program.
+	 * 
+	 * @param theArgs command line arguments - ignored
+	 */
+	public static void main(final String[] theArgs) {
+		new UI().start();
+	}
+	/**
+	 * Login prompt.  Displays prompt and scans input, compares to list of existing users.
+	 * If valid, updates this UI user fields and proceeds to top menuL chooseUserTypeMenuView 
+	 * 
+	 * @author Casey Anderson, Morgan Blackmore
+	 * 
+	 */
+
+	private void start() {
+		boolean validUserName = false;
+		while (!validUserName) {
+			System.out.println("Please enter user name to log in: ");
+			myUserName = myScanner.next();
+			if (myUser.isUserRegistered(myUserName)) {
+				validUserName = true;
+
+				chooseUserTypeMenuView();
+			} else {
+				System.out.println("Invalid User Name!");
+			}
+		}
+
+	}
+
+	/**
+	 * Top menu from login.  
+	 * User chooses how they want to proceed which notifies Observer with string
+	 * 
+	 * @author Morgan Blackmore
+	 * 
+	 */
+	private void chooseUserTypeMenuView() {
+
+		System.out.println("\nChoose what type of user you are");
+		System.out.println("1 - Author");
+		System.out.println("2 - SubProgram Chair");
+
+		if (myScanner.next()== "1") {
+			notifyObservers("AUTHOR"); 
+		} else if(myScanner.next()=="2"){
+			notifyObservers("SUBPROGRAM_CHAIR"); 
+
+			if (myScanner.next() == "1") {
+				notifyObservers("Author"); 
+			} else if(myScanner.next() =="2"){
+				notifyObservers("SubProgram Chair"); 
+
+			} else {
+				System.out.println("Invalid choice, please select from the options displayed");
+				chooseUserTypeMenuView();
+			}
+		}    	
+
+
+	}
+
 	public void changeState(int theState) {
-			
+
 		if (theState < 10) {
 			setUserType("Author");
 			displayHeader();
@@ -126,22 +126,23 @@ public class UI extends Observable implements Observer{
 		} else {
 			setUserType("SubProgramChair");
 			displayHeader();
-			
+
 			subProgramChairView();
+			subProgramChairAssignReviewerView(); 
 		}
 	}
-	
-	
+
+
 	private void setUserType(String theUserType) {
 		userType = theUserType;
 	}
-	
+
 	private void displayHeader() {
 		System.out.println(myUserName + " - " + userType);
 		System.out.println();
-		
+
 	}
-	
+
 	private void AuthorView() {
 		System.out.println("Author Options:");
 		System.out.println("1 - Submit new Manuscript.");
@@ -150,9 +151,9 @@ public class UI extends Observable implements Observer{
 		while (!myScanner.hasNextInt() || (myUserChoice < 1 || myUserChoice > 3)) {
 			myUserChoice = myScanner.nextInt();
 		}
-		
+
 	}
-	
+
 	private void AuthorManuscriptSubmissionView() {
 		String manuscriptTile;
 		String authorList = "";
@@ -177,19 +178,19 @@ public class UI extends Observable implements Observer{
 			authorList += listOfAuthors.get(i) + ",";
 		}
 		notifyObservers("Submit Manuscript," + manuscriptTile + "," + dtf.format(localDate) + authorList); 
-		
-		
+
+
 	}
-	
+
 	private void AuthorListOfManuscriptsView() {
 		System.out.println("List of currently submitted Manuscripts:");
 		for (int i = 0; i < )
 	}
-	
-	private void ListOfConferenceView() { //how know which conference list to display?
-		
+
+	private void ListOfConferenceView() { //Gets list from database and displays.  DB isn't ready yet.
+
 	}
-	
+
 	/**
 	 * Display SubProgramChair top menu
 	 * 
@@ -200,44 +201,48 @@ public class UI extends Observable implements Observer{
 		System.out.println("1 - Assign a Reviewer");
 		System.out.println("2 - Select a Conference");
 		System.out.println("3 - Select a Manuscript");
-//		while (!myScanner.hasNextInt() || (myUserChoice < 1 || myUserChoice > 3)) {
-			myUserChoice = myScanner.nextInt();
-//		}
+		//		while (!myScanner.hasNextInt() || (myUserChoice < 1 || myUserChoice > 3)) {
+		myUserChoice = myScanner.nextInt();
+		//		}
 		switch (myUserChoice){
-			case (1):
-				notifyObservers(); //add notify message
-				break;
-			case (2):
-				notifyObservers(); //add notify message
-				break;
-			case (3):
-				notifyObservers(); //add notify message
-				break;
+		case (1):
+			notifyObservers("ASSIGN_REVIEWER"); 
+		break;
+		case (2):
+			notifyObservers("LIST_CONFERENCE_VIEW"); 
+		break;
+		case (3):
+			notifyObservers("LIST_MANUSCRIPT_VIEW"); 
+		break;
 		}
-				
 
-		}
-	
-	private void myManuscriptsView() {
+
+	}
+
+	private void subProgramChairManuscriptsView() {
+
+	}
+
+	private void subProgramChairConferenceView() {
 		
 	}
-	
-	private void assignReviewerView() {
+	private void subProgramChairAssignReviewerView() {
+
 		System.out.println("Assign Reviewer Options");
 		System.out.println("First, Choose a conference");
 		ListOfConferenceView();//display conference list
-		
+
 	}
-	
-	
+
+
 
 	@Override
 	public void removeListener(InvalidationListener listener) {
 
 
 		public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+			// TODO Auto-generated method stub
 
-}
+		}
+
+	}
