47d0c823fe70b980a20a8e22f2bce3897bc111a7
MDanger <clearwaters@gmail.com>
2017-05-06 10:10:31 -0700

    minor tweaks

--- 360GroupProject/src/UI.java
+++ 360GroupProject/src/UI.java
@@ -1,4 +1,39 @@
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 
-public class UI {
+/*Shit to do:
+ * When user makes a selection, update with the string of the menu item user chose
+ * 
+*/
+/**
+ * Conference UI.
+ *  * 
+ * @author Morgan Blackmore, Casey Anderson
+ *
+ */
+public class UI implements Observable {
+	
+	
+	public void changeScreen(int theState) {
+		//when an author logs in, should see a list of conferences, which comes from User interface
+		//
+	}
+	
+	private int SubProgramChairView(){
+		return 0;
+		
+	}
+
+	@Override
+	public void addListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeListener(InvalidationListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
