8d15e21cc48122637a7dff23010578fd3e867e4a
BlueAccords <myoldemailname@gmail.com>
2017-05-06 17:18:35 -0700

    added constructor for conferenced atabase

--- 360GroupProject/src/model/ConferenceDatabase.java
+++ 360GroupProject/src/model/ConferenceDatabase.java
@@ -1,12 +1,22 @@
-/**
- * 
- */
 package model;
+
+import java.util.TreeMap;
+import java.util.UUID;
 
 /**
  * @author Ryan Tran
  *
  */
 public class ConferenceDatabase {
+
+	private TreeMap<UUID, Conference> myConferenceList;
+
+	private final String CONFERENCE_SERIALIZED_PATH = "360GroupProject/src/model/serializedModel/";
+	
+	public void AccountDatabase() {
+		this.myConferenceList = null;
+	}
+	
+	
 
 }
