b2422014cb53f484ba112b5f70d52dd7bce4e06a
BlueAccords <myoldemailname@gmail.com>
2017-05-06 17:19:29 -0700

    added empty test file for conference database

---
+++ 360GroupProject/src/tests/ConferenceDatabaseTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author BlueAccords
+ *
+ */
+public class ConferenceDatabaseTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
