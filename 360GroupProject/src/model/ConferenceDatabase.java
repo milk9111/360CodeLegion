658c1546e90d9df5ac1fbc8e413150311f0770a0
BlueAccords <myoldemailname@gmail.com>
2017-05-06 17:30:57 -0700

    added conferenceDatabase deserializatoin and creatoin of empty database

--- 360GroupProject/src/model/ConferenceDatabase.java
+++ 360GroupProject/src/model/ConferenceDatabase.java
@@ -1,5 +1,10 @@
 package model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.TreeMap;
 import java.util.UUID;
 
@@ -12,11 +17,61 @@ public class ConferenceDatabase {
 	private TreeMap<UUID, Conference> myConferenceList;
 
 	private final String CONFERENCE_SERIALIZED_PATH = "360GroupProject/src/model/serializedModel/";
-	
+	private final String CONFERENCE_FILE_PATHNAME = "conferences.ser";
+
 	public void AccountDatabase() {
 		this.myConferenceList = null;
 	}
-	
-	
+
+	public TreeMap<UUID, Conference> getAllConferences() {
+		TreeMap<UUID, Conference> listToReturn = null;
+
+		return null;
+	}
+
+
+	/**
+	 * Deserialize the conference list if it exists
+	 * @return a TreeMap of the conference list
+	 */
+	public TreeMap<UUID, Conference> deserializeConferenceList() {
+		TreeMap<UUID, Conference> conferenceListToReturn = null;
+
+		try {
+			FileInputStream fileIn = new FileInputStream(CONFERENCE_SERIALIZED_PATH + CONFERENCE_FILE_PATHNAME);
+
+			ObjectInputStream in = new ObjectInputStream(fileIn);
+			conferenceListToReturn = (TreeMap<UUID, Conference>) in.readObject();
+			in.close();
+			fileIn.close();
+
+		// return nulls on error
+		} catch(IOException i) {
+			i.printStackTrace();
+			return null;
+		} catch(ClassNotFoundException c) {
+			System.out.println("Conference List could not be found");
+			c.printStackTrace();
+			return null;
+		}
+		
+		return conferenceListToReturn;
+	}
+
+	public void createEmptySerializedConferenceList() {
+		TreeMap<UUID, Conference> emptyConferenceList = new TreeMap<UUID, Conference>();
+
+		try {
+			FileOutputStream fileOut = new FileOutputStream(CONFERENCE_SERIALIZED_PATH + CONFERENCE_FILE_PATHNAME);
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(emptyConferenceList);
+			out.close();
+			fileOut.close();
+		}catch(IOException i) {
+			i.printStackTrace();
+		}
+	}
+
+
 
 }
--- 360GroupProject/src/tests/ConferenceDatabaseTest.java
+++ 360GroupProject/src/tests/ConferenceDatabaseTest.java
@@ -5,25 +5,49 @@
 
 import static org.junit.Assert.*;
 
+import java.util.TreeMap;
+import java.util.UUID;
+
 import org.junit.Before;
 import org.junit.Test;
+
+import model.Account;
+import model.AccountDatabase;
+import model.Conference;
+import model.ConferenceDatabase;
 
 /**
  * @author BlueAccords
  *
  */
 public class ConferenceDatabaseTest {
+	
+	private ConferenceDatabase myConferenceDatabase;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		myConferenceDatabase = new ConferenceDatabase();
+		myConferenceDatabase.createEmptySerializedConferenceList();
 	}
 
+	
+	/**
+	 * Test method for {@link AccountDatabase#deserializeAccountList()}.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void deserializeAccountList_forEmptyList_shouldReturnEmptyList() {
+		TreeMap<UUID, Conference> validList = myConferenceDatabase.deserializeConferenceList();
+		assertTrue(validList instanceof TreeMap);
+	}
+	
+	@Test
+	public void createEmptySerializedAccountList_ForNoListAvailable_ShouldCreateNewList() {
+		myConferenceDatabase.createEmptySerializedConferenceList();
+		TreeMap<UUID, Conference> validList = myConferenceDatabase.deserializeConferenceList();
+		assertTrue(validList.size() == 0);
 	}
 
 }
